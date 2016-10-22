ee511318d917f8d3367dfc5b267699c61cf21629
jmurkoth <jmurkoth@gmail.com>
2016-10-22 08:09:47 -0500

    First cut with custom exception handling

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,19 @@
 // entry point to the application
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule  }   from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 // custom component import
 import {AppComponent} from './app.component';
 import {HeaderComponent } from './shared/header.component';
 import {FooterComponent} from './shared/footer.component';
+import {HomeComponent} from './home/home.component';
 
+import{CustomToastrErrorHandler} from './common/customtoastr.errorhandler';
 @NgModule({
     imports: [BrowserModule, FormsModule],
     exports: [],
-    declarations: [AppComponent, HeaderComponent, FooterComponent],
-    providers: [],
+    declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
+    providers: [{provide: ErrorHandler, useClass: CustomToastrErrorHandler}],
     bootstrap: [AppComponent]
 })
 
---
+++ app/common/customtoastr.errorhandler.ts
@@ -0,0 +1,49 @@
+import { ErrorHandler  } from '@angular/core';
+import {WrappedError} from '@angular/core/src/facade/errors';
+
+export class CustomToastrErrorHandler implements ErrorHandler {
+
+ constructor() {
+     toastr.options = { positionClass: 'toast-bottom-right' , closeButton: true};
+ }
+
+  handleError(error: any): void {
+    const originalError = this._findOriginalError(error);
+    const originalStack = this._findOriginalStack(error);
+    let exceptionValue = `ORIGINAL EXCEPTION: ${this._extractMessage(originalError)}`;
+    let stackTraceValue = `ORIGINAL STACKTRACE: ${originalStack}`;
+    toastr.error(exceptionValue);
+    toastr.error(stackTraceValue);
+    console.error(exceptionValue );
+    console.error(stackTraceValue );
+  }
+
+ _extractMessage(error: any): string {
+    return error instanceof Error ? error.message : error.toString();
+  }
+
+  _findOriginalError(error: any): any {
+    let e = (error as WrappedError).originalError;
+    while (e && (e as WrappedError).originalError) {
+      e = (e as WrappedError).originalError;
+    }
+    return e;
+  }
+  _findOriginalStack(error: any): string {
+    if (!(error instanceof Error)) {
+    return null;
+    }
+
+    let e: any = error;
+    let stack: string = e.stack;
+    while (e instanceof Error && (e as WrappedError).originalError) {
+      e = (e as WrappedError).originalError;
+      if (e instanceof Error && e.stack) {
+        stack = e.stack;
+      }
+    }
+
+    return stack;
+  }
+}
+
