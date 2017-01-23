9a5139744334155715b775d128b02ca3eeed3e1e
jmurkoth <jmurkoth@gmail.com>
2017-01-22 22:16:42 -0600

    added missing css

--- app/vendor.ts
+++ app/vendor.ts
@@ -14,6 +14,7 @@ import '../node_modules/jquery/src/jquery';
 import '../node_modules/bootstrap/dist/js/bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.css';
+import '../node_modules/ng2-toastr/bundles/ng2-toastr.min.css';
 
 import  'moment/moment';
 import  'ng2-toastr/ng2-toastr';
